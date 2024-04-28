af2ae38345eb82434b7d4c2fb7e926170400b27c
GAkl224 <156771707+GAkl224@users.noreply.github.com>
2024-04-28 13:00:40 -0500

    update - (Three Credits) Array Indexing

--- main.cpp
+++ main.cpp
@@ -4,47 +4,56 @@
 using namespace std;
 
 class Point {
-    private:
-        double x;
-        double y;
+private:
+    double x;
+    double y;
 
-    public:
-        Point(double xCd, double yCd) : x(xCd), y(yCd) {
+public:
+    Point(double xCd, double yCd) : x(xCd), y(yCd) {}
 
-        }
+    double getX() const {
+        return x;
+    }
 
-        double getX() const {
+    double getY() const {
+        return y;
+    }
+
+    //overloading operator
+    double operator[](char axis) const {
+        if (axis == 'x' || axis == 'X') {
             return x;
-        }
-
-        double getY() const {
+        } else if (axis == 'y' || axis == 'Y') {
             return y;
+        } else {
+            throw invalid_argument("Invalid axis. Use 'x' or 'y'.");
         }
+    }
 
-        double distance(const Point& other) const {
-            return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
-        }
+    double distance(const Point& other) const {
+        return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
+    }
 
-        bool operator==(const Point& other) const {
-            return x == other.x && y == other.y;
-        }
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
 
-        bool operator!=(const Point& other) const {
-            return !(*this == other);
-        }
+    bool operator!=(const Point& other) const {
+        return !(*this == other);
+    }
 
-        Point operator-(const Point& other) const {
-            return Point(fabs(x - other.x), fabs(y - other.y));
-        }
+    Point operator-(const Point& other) const {
+        return Point(fabs(x - other.x), fabs(y - other.y));
+    }
 
-        Point operator/(const Point& other) const {
-            return Point((x + other.x) /2, (y + other.y) /2);
-        }
+    Point operator/(const Point& other) const {
+        return Point((x + other.x) / 2, (y + other.y) / 2);
+    }
 };
 
-//driver
+// Driver
 int main() {
-    //point class objects
+    
     Point p1 = Point(1.45, 2.2);
     Point p2 = Point(30.23, 4.5);
 
@@ -52,10 +61,10 @@ int main() {
     cout << "Distance between p1 and p2: " << distance.distance(Point(0, 0)) << endl;
 
     cout << "p1 == p2: " << boolalpha << (p1 == p2) << endl;
-    cout << "p1 != p2: " << boolalpha <<(p1 != p2) << endl;
+    cout << "p1 != p2: " << boolalpha << (p1 != p2) << endl;
 
     Point midpoint = p1 / p2;
-    cout << "midpoint p1 & p2: (" << midpoint.getX() << "," << midpoint.getY() << ")" << endl;
+    cout << "Midpoint p1 & p2: (" << midpoint['x'] << "," << midpoint['y'] << ")" << endl;
 
     return 0;
-}
+}   
